cdcfd20b5c149c1fcab2e6174c9b5f1d554fb3e4
Scott Deboy <sdeboy@apache.org>
2010-04-30 20:36:39 +0000

    Undo last regexp build change to logfilepatternreceiver (was correct)
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/companions/receivers/trunk@939808 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/log4j/varia/LogFilePatternReceiver.java
+++ src/main/java/org/apache/log4j/varia/LogFilePatternReceiver.java
@@ -709,10 +709,11 @@ public class LogFilePatternReceiver extends Receiver {
     newPattern = util.substitute("s/" + MULTIPLE_SPACES_REGEXP +"/" + MULTIPLE_SPACES_REGEXP + "/g", newPattern);
     newPattern = globalReplace(PATTERN_WILDCARD, REGEXP_DEFAULT_WILDCARD, newPattern);
 
-    for (int i = 0;i<matchingKeywords.size();i++) {
-      String keyword = (String) matchingKeywords.get(i);
+    //use buildingKeywords here to ensure correct order
+    for (int i = 0;i<buildingKeywords.size();i++) {
+      String keyword = (String) buildingKeywords.get(i);
       //make the final keyword greedy (we're assuming it's the message)
-      if (i == (matchingKeywords.size() - 1)) {
+      if (i == (buildingKeywords.size() - 1)) {
         newPattern = singleReplace(newPattern, String.valueOf(i), GREEDY_GROUP);
       } else if (TIMESTAMP.equals(keyword)) {
         newPattern = singleReplace(newPattern, String.valueOf(i), "(" + timestampPatternText + ")");
