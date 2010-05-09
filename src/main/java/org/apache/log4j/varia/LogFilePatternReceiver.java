c80ac157ce577fa95b8dfb2f561a0ad3314c71ef
Scott Deboy <sdeboy@apache.org>
2010-05-09 08:30:00 +0000

    Added ability to highlight search match text in the table (per-log panel preference, enabled by default)  - modified the Rule.evaluate method to accept a Map argument.  If the map argument is not null, the map will be populated with text matches resulting from the rule evaluation - the keys are the field names containing text matches discovered during rule evaluation, the values are the (possibly multiple) text matches - updated Chainsaw & receivers pom.xml to require log4j-extras 1.1 snapshot due to the change to the Rule API  - renamed JEditorPaneFormatter to JTextComponentFormatter  - modified the Search match color in the table from black background to grey background, from white foreground to black foreground - reworked concurrency & use of event lists in ChainsawCyclicBufferTableModel - added search matches to extendedloggingevent (used during rendering) - updated findnext/previous logic to handle no selected row, better handling of wrapping forward & back - reworked tablecellrenderer logic to support bolding of search-match text in the table (required changing multiline fields to use a jtextpane) - updated all Rules to accept a Map which is populated by String keys representing the fields providing a text match, and Set values containing the one or more text matches for that field (if the map is null, no collection of results is performed) - updated And, Or and Not rules to correctly aggregate text match results of their child rules - updated ExpressionRule to allow definition of expressions containing keywords as text (must be single-quoted)
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/companions/receivers/trunk@942512 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/log4j/varia/LogFilePatternReceiver.java
+++ src/main/java/org/apache/log4j/varia/LogFilePatternReceiver.java
@@ -566,7 +566,7 @@ public class LogFilePatternReceiver extends Receiver {
   private boolean passesExpression(LoggingEvent event) {
     if (event != null) {
       if (expressionRule != null) {
-        return (expressionRule.evaluate(event));
+        return (expressionRule.evaluate(event, null));
       }
     }
     return true;
